e59865f49ae4fffb495d1a8ca097e39faebab66a
Pebble <saival2003@gmail.com>
2024-03-15 22:18:27 +0300

    first commit

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrEmpty(number1.Text) || String.IsNullOrEmpty(number2.Text))
+            {
+                MessageBox.Show("Заполните поля");
+                return;
+            }
+
+            try
+            {
+                equals.Content = Int32.Parse(number1.Text) + Int32.Parse(number2.Text);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Неверный тип вводимых данных");
+                return;
+            }
+        }
+
+        private void ButtonExit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
